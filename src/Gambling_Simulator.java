5cc5e617d0ca0e802ba2ef2009d09e1baea0b767
KunigiMohan <kmohanr5@gmail.com>
2021-10-31 23:14:14 +0530

    Added : Implementing Game resign condition for a Gambler

--- src/Gambling_Simulator.java
+++ src/Gambling_Simulator.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.util.Random;
+
 public class Gambling_Simulator {
     public static void main (String[] args){
-        int stake_of_Gambler= 100;              //Intialising amount present in stake_of_Gambler
+        int stake_of_Gambler= 100;                  //Intialising amount present with Gambler before starting Game in stake_of_Gambler
         int bet_for_Game = 1;                       //Intialising how much amount gamer bet in bet_for_Game
-        Random ran = new Random();
-        int i = ran.nextInt(2);                 //generating random number to check Gamer win loose the Game
-
+        int count =1;
         /*
-         *   Declaring condition for Gamer to win in if condition
+         * Implementing condition given in UC3 i.e winning of Half of his stake using while loop
          * */
-
-        if (i==0){
-            System.out.println("Player won the game");
-            stake_of_Gambler=stake_of_Gambler+bet_for_Game;         //Adding amount to Gamer stake after hi wins
+        while(stake_of_Gambler <=149 && stake_of_Gambler>=51)
+        {
+            Random ran = new Random();
+            int i = ran.nextInt(2);             //generating random number to check Gamer win loose the Game
+            if (i == 0) {
+                stake_of_Gambler = stake_of_Gambler + bet_for_Game;     //Adding amount to Gamer stake after hi wins
+                count++;
+            }
+            else {
+                stake_of_Gambler = stake_of_Gambler - bet_for_Game;     //Removing amount loosen by Gamer when he loose the Game
+                count++;
+            }
         }
-        else{
-            System.out.println("Player loss the Game");
-            stake_of_Gambler=stake_of_Gambler-bet_for_Game;         //Removing amount loosen by Gamer when he loose the Game
-        }
-
-    }
+        System.out.println("The amount at Gamer after end of Game : "+stake_of_Gambler);//Total stake left with Gambler
+    }                                                                                     //at end of the day
 }
